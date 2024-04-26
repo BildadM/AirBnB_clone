9cd91355a1bf74e538ad9e61a1290efd86529a43
wanjikumutuku <moreenkinyua4@gmail.com>
2024-04-26 01:56:54 -0700

    corrected task 4

--- models/base_model.py
+++ models/base_model.py
@@ -6,10 +6,17 @@ from datetime import datetime
 
 
 class BaseModel:
-    def __init__(self):
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.now()
-            self.updated_at = self.created_at
+    def __init__(self, *args, **kwargs):
+        timeform = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.now()
+        self.updated_at = self.created_at
+        if len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, timeform)
+                else:
+                        self.__dict__[k] = v
 
     def __str__(self):
         return "[{}] ({}) {}".format(self.__class__.__name__,
