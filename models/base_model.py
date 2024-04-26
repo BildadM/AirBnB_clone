900c3307fedfdb33218748b799fc0bf0f8fc05f6
wanjikumutuku <moreenkinyua4@gmail.com>
2024-04-26 01:20:14 -0700

    Corrected base_model task 3

--- models/base_model.py
+++ models/base_model.py
@@ -6,16 +6,7 @@ from datetime import datetime
 
 
 class BaseModel:
-    def __init__(self, *args, **kwargs):
-        if kwargs:
-            kwargs["created_at"] = datetime.strptime
-            (kwargs["created_at"], "%Y-%m-%dT%H:%M:%S.%f")
-            kwargs["updated_at"] = datetime.strptime
-            (kwargs["updated_at"], "%Y-%m-%dT%H:%M:%S.%f")
-            for key, value in kwargs.items():
-                if key != "__class__":
-                    setattr(self, key, value)
-        else:
+    def __init__(self):
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
